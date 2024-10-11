012a2b1997ea52b9f26a51cb42c3ea8f99c80ca5
Mohammed Mohalil Naikwadi <mohalil.naikwadi@agilepoint.com>
2024-10-11 20:27:29 +0530

    [Test]

--- src/Server.cs
+++ src/Server.cs
@@ -8,8 +8,22 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
-
-var clientSocket = server.AcceptSocket(); // wait for client
-//await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
-clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+//server.AcceptSocket(); // wait for client
+Console.WriteLine("Server started, waiting for a client...");
+while (true)
+{
+    var clientSocket = server.AcceptSocket(); // wait for client
+    Console.WriteLine("Client connected.");
+    await HandleClient(clientSocket);
+}
+static async Task HandleClient(Socket clientSocket)
+{
+    var buffer = new byte[1024];
+    int bytesRead = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+    var receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    Console.WriteLine($"Received: {receivedMessage}");
+    var response = Encoding.UTF8.GetBytes("+PONG\r\n");
+    await clientSocket.SendAsync(response, SocketFlags.None);
+    clientSocket.Shutdown(SocketShutdown.Both);
+    clientSocket.Close();
+}
